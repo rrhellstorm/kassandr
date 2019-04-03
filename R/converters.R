a172bbf7fd8ff5b86a98b55f2abc4b2ca87174c5
Boris Demeshev <boris.demeshev@gmail.com>
2019-04-03 23:12:11 +0300

    dplyr::

--- R/converters.R
+++ R/converters.R
@@ -505,7 +505,7 @@ convert_invest_xlsx <- function(path_to_source = "http://www.gks.ru/bgd/regl/b19
   data_vector <- data[4:23, 3:6]  %>% t() %>% as.vector()
   colnames(data_vector) <- NULL
   data_ts <- stats::ts(data_vector, start = c(1999, 1), freq = 4)
-  data_tsibble <- tsibble::as_tsibble(data_ts)%>% rename(date = index, invest = value)
+  data_tsibble <- tsibble::as_tsibble(data_ts) %>% dplyr::rename(date = index, invest = value)
   data_tsibble = dplyr::mutate(data_tsibble, access_date=access_date, date = as.Date(date))
   check_conversion(data_tsibble)
   return(data_tsibble)
