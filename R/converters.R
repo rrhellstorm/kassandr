8f845688f8a2d490124638c3fec6af23ee30c40c
Oxana Malakhovskaya <oxana.malakhovskaya@gmail.com>
2020-06-15 05:47:22 +0300

    update converters as link for the data has changed

--- R/converters.R
+++ R/converters.R
@@ -327,13 +327,14 @@ convert_m2_m2_sa_xlsx <- function(path_to_source =
 #' \donttest{
 #' ind = convert_ind_okved2_xlsx()
 #' }
+#' Old link for data: http://www.gks.ru/storage/mediabank/ind_okved2(1).xlsx
 convert_ind_okved2_xlsx <- function(path_to_source = 
-                                      "http://www.gks.ru/storage/mediabank/ind_okved2(1).xlsx", 
+                                      "https://gks.ru/storage/mediabank/ind-baza-2018.xlsx", 
                                     access_date = Sys.Date()) {
   indprod <- rio::import(path_to_source, skip = 2, sheet = 1)
   indprod_vector <- t(indprod[2, 3:ncol(indprod)])
-  
-  indprod_ts <- stats::ts(indprod_vector, start = c(2013, 1), frequency = 12)
+  # for periods before January, 2020 use old link for the data and start = c(2015, 1)
+  indprod_ts <- stats::ts(indprod_vector, start = c(2015, 1), frequency = 12)
   indprod_tsibble <- tsibble::as_tsibble(indprod_ts)
   indprod_tsibble = dplyr::rename(indprod_tsibble, date = index, ind_prod = value)
   indprod_tsibble = dplyr::mutate(indprod_tsibble, access_date = access_date)
